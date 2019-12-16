75d131349c792979136e84da3a8fd5771982e0c7
ayan4m1 <andrew@bulletlogic.com>
2019-12-16 01:50:36 -0500

    rebuild compiled code using new toolchain

--- dist/cache.js
+++ dist/cache.js
@@ -9,8 +9,6 @@ exports["default"] = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
@@ -37,87 +35,67 @@ function () {
     }
   }, {
     key: "store",
-    value: function () {
-      var _store = (0, _asyncToGenerator2["default"])(
-      /*#__PURE__*/
-      _regenerator["default"].mark(function _callee(query, results, duration) {
-        var hash;
-        return _regenerator["default"].wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                hash = this.hash(query);
-                _context.next = 3;
-                return cache.set(hash, JSON.stringify(results));
+    value: function store(query, results, duration) {
+      var hash;
+      return _regenerator["default"].async(function store$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              hash = this.hash(query);
+              _context.next = 3;
+              return _regenerator["default"].awrap(cache.set(hash, JSON.stringify(results)));
 
-              case 3:
-                cache.expire(hash, duration);
+            case 3:
+              cache.expire(hash, duration);
 
-              case 4:
-              case "end":
-                return _context.stop();
-            }
+            case 4:
+            case "end":
+              return _context.stop();
           }
-        }, _callee, this);
-      }));
-
-      function store(_x, _x2, _x3) {
-        return _store.apply(this, arguments);
-      }
-
-      return store;
-    }()
+        }
+      }, null, this);
+    }
   }, {
     key: "get",
-    value: function () {
-      var _get = (0, _asyncToGenerator2["default"])(
-      /*#__PURE__*/
-      _regenerator["default"].mark(function _callee2(query) {
-        var hash, exists;
-        return _regenerator["default"].wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                hash = this.hash(query);
-                _context2.next = 3;
-                return cache.exists(hash);
+    value: function get(query) {
+      var hash, exists;
+      return _regenerator["default"].async(function get$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              hash = this.hash(query);
+              _context2.next = 3;
+              return _regenerator["default"].awrap(cache.exists(hash));
 
-              case 3:
-                exists = _context2.sent;
+            case 3:
+              exists = _context2.sent;
 
-                if (!exists) {
-                  _context2.next = 10;
-                  break;
-                }
-
-                _context2.next = 7;
-                return cache.get(hash);
-
-              case 7:
-                _context2.t0 = _context2.sent;
-                _context2.next = 11;
+              if (!exists) {
+                _context2.next = 10;
                 break;
+              }
 
-              case 10:
-                _context2.t0 = null;
+              _context2.next = 7;
+              return _regenerator["default"].awrap(cache.get(hash));
 
-              case 11:
-                return _context2.abrupt("return", _context2.t0);
+            case 7:
+              _context2.t0 = _context2.sent;
+              _context2.next = 11;
+              break;
 
-              case 12:
-              case "end":
-                return _context2.stop();
-            }
+            case 10:
+              _context2.t0 = null;
+
+            case 11:
+              return _context2.abrupt("return", _context2.t0);
+
+            case 12:
+            case "end":
+              return _context2.stop();
           }
-        }, _callee2, this);
-      }));
-
-      function get(_x4) {
-        return _get.apply(this, arguments);
-      }
-
-      return get;
-    }()
+        }
+      }, null, this);
+    }
   }]);
   return Cache;
 }();
--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,6 @@ exports["default"] = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
@@ -30,89 +28,69 @@ function () {
 
   (0, _createClass2["default"])(PgRedis, [{
     key: "prepare",
-    value: function () {
-      var _prepare = (0, _asyncToGenerator2["default"])(
-      /*#__PURE__*/
-      _regenerator["default"].mark(function _callee(text, options) {
-        return _regenerator["default"].wrap(function _callee$(_context) {
-          while (1) {
-            switch (_context.prev = _context.next) {
-              case 0:
-                return _context.abrupt("return", this.db.prepare(text, options));
+    value: function prepare(text, options) {
+      return _regenerator["default"].async(function prepare$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              return _context.abrupt("return", this.db.prepare(text, options));
 
-              case 1:
-              case "end":
-                return _context.stop();
-            }
+            case 1:
+            case "end":
+              return _context.stop();
           }
-        }, _callee, this);
-      }));
-
-      function prepare(_x, _x2) {
-        return _prepare.apply(this, arguments);
-      }
-
-      return prepare;
-    }()
+        }
+      }, null, this);
+    }
   }, {
     key: "query",
-    value: function () {
-      var _query = (0, _asyncToGenerator2["default"])(
-      /*#__PURE__*/
-      _regenerator["default"].mark(function _callee2(text) {
-        var options,
-            cacheHit,
-            statement,
-            results,
-            _args2 = arguments;
-        return _regenerator["default"].wrap(function _callee2$(_context2) {
-          while (1) {
-            switch (_context2.prev = _context2.next) {
-              case 0:
-                options = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : {
-                  duration: 0
-                };
-                cacheHit = this.cache.get(text);
+    value: function query(text) {
+      var options,
+          cacheHit,
+          statement,
+          results,
+          _args2 = arguments;
+      return _regenerator["default"].async(function query$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              options = _args2.length > 1 && _args2[1] !== undefined ? _args2[1] : {
+                duration: 0
+              };
+              cacheHit = this.cache.get(text);
 
-                if (!cacheHit) {
-                  _context2.next = 6;
-                  break;
-                }
+              if (!cacheHit) {
+                _context2.next = 6;
+                break;
+              }
 
-                return _context2.abrupt("return", cacheHit);
+              return _context2.abrupt("return", cacheHit);
 
-              case 6:
-                _context2.next = 8;
-                return this.db.prepare(text, options);
+            case 6:
+              _context2.next = 8;
+              return _regenerator["default"].awrap(this.db.prepare(text, options));
 
-              case 8:
-                statement = _context2.sent;
-                _context2.next = 11;
-                return statement.execute();
+            case 8:
+              statement = _context2.sent;
+              _context2.next = 11;
+              return _regenerator["default"].awrap(statement.execute());
 
-              case 11:
-                results = _context2.sent;
+            case 11:
+              results = _context2.sent;
 
-                if (options.duration > 0) {
-                  this.cache.store(text, results.rows, options.duration);
-                }
+              if (options.duration > 0) {
+                this.cache.store(text, results.rows, options.duration);
+              }
 
-                return _context2.abrupt("return", results.rows);
+              return _context2.abrupt("return", results.rows);
 
-              case 14:
-              case "end":
-                return _context2.stop();
-            }
+            case 14:
+            case "end":
+              return _context2.stop();
           }
-        }, _callee2, this);
-      }));
-
-      function query(_x3) {
-        return _query.apply(this, arguments);
-      }
-
-      return query;
-    }()
+        }
+      }, null, this);
+    }
   }]);
   return PgRedis;
 }();
